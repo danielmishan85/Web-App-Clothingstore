4c0f9fec3fa237c5977d070a8efab4bb7819d1c6
danielmishan85 <danielmishan85@gmail.com>
2023-01-05 18:20:04 +0200

    cleaning code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./components/HomePage/HomePageContent";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CategoryCard from "./components/Category/CategoryCard";
 import products from "./data";
 import CategoryItems from "./components/Card/CategoryItems";
-import ProductCard from "./components/Products/ProductCard";
 import ProductPage from "./components/Products/ProductPage";
 import SigninPage from "./components/Profile/SigninPage";
 import SignUpPage from "./components/Profile/SignupPage";
+
+
 function App() {
   return (
     <Router>
