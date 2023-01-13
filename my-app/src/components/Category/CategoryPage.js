8a7bbd99eb2554765ac0c2423de538dae70781cb
Eden Shviro <103212615+edenshviro82@users.noreply.github.com>
2023-01-13 13:05:45 +0200

    Add files via upload
    add category page

---
+++ my-app/src/components/Category/CategoryPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CategorySection from "./CategorySection";
+
+const CategoryPage = () => {
+  return (
+    <div>
+      <h1>Brands</h1>
+
+      <CategorySection></CategorySection>
+    </div>
+  );
+};
+
+export default CategoryPage;
