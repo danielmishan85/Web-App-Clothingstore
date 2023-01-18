a5076c28d7fa9aa993487117a526311ffee0090b
LitalHekmati <100677022+LitalHekmati@users.noreply.github.com>
2023-01-18 13:49:04 +0200

    start to work on navbar change when login happened

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,23 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import HomePage from './components/HomePage/HomePageContent';
+import React, { useEffect, useState, useCallback, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import products from './data';
-import CategoryItems from './components/Card/CategoryItems';
-import ProductPage from './components/Products/ProductPage';
-import SigninPage from './components/Profile/SigninPage';
-import SignUpPage from './components/Profile/SignupPage';
-import ForgotPasswordPage from './components/Profile/ForgotPasswordPage';
-import React from 'react';
-import { useEffect, useState } from 'react';
-import CategoryPage from './components/Category/CategoryPage';
+
+import Home from './pages/Home';
+import User from './pages/User';
+import Categories from './pages/Categories';
+import Category from './pages/Category';
+import Product from './pages/Product';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import MainNavBar from './components/Navigation/MainNavigation';
+import { AuthContext } from './components/context/auth-context';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [products, setProducts] = useState([]);
+  const auth = useContext(AuthContext);
+  console.log(isLoggedIn);
+  console.log(auth.isLoggedIn);
 
   const sendRequest = () => {
     fetch('http://localhost:5000/api/products')
@@ -26,28 +30,40 @@ function App() {
     sendRequest();
   }, []);
 
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+    console.log(isLoggedIn);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    console.log(isLoggedIn);
+  }, []);
+
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signin' element={<SigninPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/passwordReset' element={<ForgotPasswordPage />} />
-        <Route
-          path='/:title'
-          element={<CategoryItems categoryItems={products} />}
-        />
-        <Route
-          path='/products/:productId'
-          element={<ProductPage products={products} />}
-        />
-        <Route
-          path='/categories'
-          element={<CategoryPage categories={products} />}
-        />
-      </Routes>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedin: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavBar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/profile' element={<User />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/passwordReset' element={<ForgotPassword />} />
+          <Route path='/brands' element={<Categories />} />
+          <Route
+            path='/:title'
+            element={<Category categoryItems={products} />}
+          />
+          <Route
+            path='/products/:productId'
+            element={<Product products={products} />}
+          />
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
